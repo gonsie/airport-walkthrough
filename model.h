28a0b587c1488f6937d98fa6b5ebd6377a5870c7
Elsa Gonsiorowski (Uranus) <me@gonsie.com>
2016-03-28 19:21:04 -0400

    added model.h code

---
+++ model.h
@@ -0,0 +1,34 @@
+#ifndef __AIRPORT_H__
+#define __AIRPORT_H__
+
+#include <ross.h>
+
+typedef enum {
+	FLIGHT,
+	RUNWAY,
+	GROUND,
+} airplane_status;
+
+typedef struct {
+	airplane_status status;
+	tw_lpid current_airport;
+	tw_lpid destination_airport;
+} airplane_state;
+
+typedef struct {
+	int runway_free_flag;
+	tw_lpid * airplanes_landing;
+	tw_lpid * airplanes_taking_off;
+} airport_state;
+
+typedef enum {
+	RUNWAY_REQUEST,
+	RUNWAY_GRANT,
+	RUNWAY_FREE,
+} message_type;
+
+typedef struct {
+	message_type type;
+} message;
+
+#endif
