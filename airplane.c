def93b7327be30b76119790aa75b14434a8b7ff0
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2016-06-12 09:37:19 -0400

    added airplane pre-run

--- airplane.c
+++ airplane.c
@@ -4,7 +4,9 @@
 int g_total_airplanes = 1;
 
 void airplane_init(airplane_state *s, tw_lp *lp) {
+}
 
+void airplane_pre_run(airplane_state *s, tw_lp *lp) {
 }
 
 void airplane_event(airplane_state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
