ad2198747f329b659c8d4053767aa4f7e8079495
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2016-06-12 09:37:35 -0400

    initialized airplane LP state

--- airplane.c
+++ airplane.c
@@ -4,6 +4,9 @@
 int g_total_airplanes = 1;
 
 void airplane_init(airplane_state *s, tw_lp *lp) {
+    s->status = GROUND;
+    s->current_airport = tw_rand_integer(lp->rng, 0, g_total_airports);
+    s->destination_airport = tw_rand_integer(lp->rng, 0, g_total_airports);
 }
 
 void airplane_pre_run(airplane_state *s, tw_lp *lp) {
