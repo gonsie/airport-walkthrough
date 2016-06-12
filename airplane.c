52f9543ebc164dc76c96fa6a9d6ef5b8833d102f
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2016-06-11 22:21:37 -0400

    basic airplane airport c files

---
+++ airplane.c
@@ -0,0 +1,21 @@
+#include "model.h"
+
+// default value
+int g_total_airplanes = 1;
+
+void airplane_init(airplane_state *s, tw_lp *lp) {
+
+}
+
+void airplane_event(airplane_state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
+
+}
+
+void airplane_reverse(airplane_state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
+
+}
+
+void airplane_final(airplane_state *s, tw_lp *lp) {
+
+}
+
---
+++ airport.c
@@ -0,0 +1,20 @@
+#include "model.h"
+
+// default value
+int g_total_airports = 1;
+
+void airport_init(airplane_state *s, tw_lp *lp) {
+
+}
+
+void airport_event(airplane_state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
+
+}
+
+void airport_reverse(airplane_state *s, tw_bf *bf, message *in_msg, tw_lp *lp) {
+
+}
+
+void airport_final(airplane_state *s, tw_lp *lp) {
+
+}
