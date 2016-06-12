03c78dbbb8753b5caf380dfd3a95a05c72819a21
Elsa Gonsiorowski (Eris) <gonsie@me.com>
2016-06-12 09:56:03 -0400

    whitespace (tabs->spaces)

--- model.h
+++ model.h
@@ -4,30 +4,30 @@
 #include <ross.h>
 
 typedef enum {
-	FLIGHT,
-	GROUND,
+    FLIGHT,
+    GROUND,
 } airplane_status;
 
 typedef struct {
-	airplane_status status;
-	tw_lpid current_airport;
-	tw_lpid destination_airport;
+    airplane_status status;
+    tw_lpid current_airport;
+    tw_lpid destination_airport;
 } airplane_state;
 
 typedef struct {
-	int runway_free_flag;
-	tw_lpid * airplanes_landing;
-	tw_lpid * airplanes_taking_off;
+    int runway_free_flag;
+    tw_lpid * airplanes_landing;
+    tw_lpid * airplanes_taking_off;
 } airport_state;
 
 typedef enum {
-	RUNWAY_REQUEST,
-	RUNWAY_GRANT,
-	RUNWAY_FREE,
+    RUNWAY_REQUEST,
+    RUNWAY_GRANT,
+    RUNWAY_FREE,
 } message_type;
 
 typedef struct {
-	message_type type;
+    message_type type;
 } message;
 
 // GLOBAL VARIABLES
